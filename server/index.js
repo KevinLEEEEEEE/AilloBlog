13e8c1a8867fde2819e244d9cd8e7ecafbac13ab
Kevin <1069440019@qq.com>
2019-01-15 15:00:15 +0800

    deploy

--- server/index.js
+++ server/index.js
@@ -5,12 +5,13 @@ const serve = require('koa-static');
 const cors = require('koa2-cors');
 const path = require('path');
 
+const PORT = 80;
 const staticPath = path.resolve(__dirname, '../static');
 
 app.use(cors());
 
 app.use(serve(staticPath));
 
-app.listen(80, () => {
+app.listen(PORT, () => {
   console.log('listening on http://localhost:80');
 });
